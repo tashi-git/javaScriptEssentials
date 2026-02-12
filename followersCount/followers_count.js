6afdcee3c3dcc9c237472439cdc5309f45600817
tashi-git <tashi.github@gmail.com>
2026-02-12 05:38:31 +0530

    Add: followersCount

---
+++ followersCount/followers_count.js
@@ -0,0 +1,18 @@
+let count = 0; // Initialize count to 0
+
+function increaseCount(){
+    count++;
+    displayCount();
+    checkCountValue();
+}
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML=count
+
+}
+function checkCountValue() {
+  if (count === 10) {
+    alert("Your Instagram post gained 10 followers! Congratulations!");
+  } else if (count === 20) {
+    alert("Your Instagram post gained 20 followers! Keep it up!");
+  }
+}
